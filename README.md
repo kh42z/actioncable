6a60eef1a21d7ec086ad143ae8a8deb5da0c8162
kh42z <tterrail@student.42.fr>
2021-04-24 17:50:28 +0200

    subscribe_test

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![action-cable](https://github.com/kh42z/actioncable-client/actions/workflows/workflow.yml/badge.svg)](https://github.com/kh42z/actioncable-client/actions/workflows/workflow.yml)
+[![Coverage Status](https://coveralls.io/repos/github/kh42z/actioncable-client/badge.svg?branch=master)](https://coveralls.io/github/kh42z/actioncable-client?branch=master)
 # actioncable-client
 
 A Go Client for Rails websocket: ActionCable!
---
+++ subscribe_test.go
@@ -0,0 +1,32 @@
+package actioncable
+
+import "testing"
+
+func TestClient_Subscribe(t *testing.T) {
+	tests := map[string]struct {
+		name    string
+		id      int
+		content string
+		want    string
+	}{
+		"UserChannel 1": {name: "UserChannel",
+			id:      1,
+			want:    "{\"command\":\"subscribe\",\"identifier\":\"{\\\"channel\\\":\\\"UserChannel\\\",\\\"id\\\":1}\"}"},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			rw := &wsMock{
+				WriteLimit: 1,
+				NoRead:     true,
+			}
+			c := NewClient(rw)
+			go c.Start()
+			c.Subscribe("UserChannel", 1)
+			if string(rw.WritePayload) != tc.want {
+				t.Errorf("expecting payload to be [%s] got [%s]", tc.want, rw.WritePayload)
+			}
+			rw.CancelRead()
+		})
+	}
+}
