1d65e48bbd479691cf80628598d81483ff8dff1a
kh42z <tterrail@student.42.fr>
2021-04-27 19:13:04 +0200

    Assert disconnect

---
+++ reveive_test.go
@@ -0,0 +1,36 @@
+package actioncable
+
+import (
+	"testing"
+)
+
+func TestClient_receive(t *testing.T) {
+	tests := map[string]struct {
+		payload string
+		want    string
+	}{
+		"welcome": {
+			payload: "{\"type\":\"welcome\"}",
+			want:    "Done"},
+		"ping": {
+			payload: "{\"type\":\"ping\",\"message\":1619511683}",
+			want:    "Done",
+		},
+		"disconnect": {
+			payload: "{\"type\":\"disconnect\"}",
+			want:    "disconnect"},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			rw := &wsMock{
+				ReadLimit:   1,
+				ReadPayload: []byte(tc.payload),
+			}
+			c := NewClient(rw)
+			if err := c.Run(); err != nil && err.Error() != tc.want {
+				t.Fatal("got an unexpected error ", err)
+			}
+		})
+	}
+}
