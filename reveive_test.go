b5853fcc248b01c61e6b6efc3e074468e91dd1a8
kh42z <tterrail@student.42.fr>
2021-04-28 10:57:47 +0200

    test if first msg is not welcome

--- reveive_test.go
+++ reveive_test.go
@@ -7,30 +7,38 @@ import (
 func TestClient_receive(t *testing.T) {
 	tests := map[string]struct {
 		payload string
+		welcomed bool
 		want    string
 	}{
-		"welcome": {
-			payload: "{\"type\":\"welcome\"}",
+		"unexpected type": {
+			payload: "{\"type\":\"unexpected\"}",
+			welcomed: false,
 			want:    "Done"},
 		"ping": {
 			payload: "{\"type\":\"ping\",\"message\":1619511683}",
+			welcomed: false,
 			want:    "Done",
 		},
 		"disconnect": {
 			payload: "{\"type\":\"disconnect\"}",
+			welcomed: false,
 			want:    "disconnect"},
+		"unexpected first message": {
+			payload: "{\"type\":\"hello\"}",
+			welcomed: true,
+			want:    "expecting welcome type message"},
 	}
 
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
 			rw := &wsReadMock{
-				Welcomed:    false,
+				Welcomed:    tc.welcomed,
 				ReadLimit:   1,
 				ReadPayload: []byte(tc.payload),
 			}
 			c := NewClient(rw)
 			if err := c.Run(); err != nil && err.Error() != tc.want {
-				t.Fatal("got an unexpected error ", err)
+				t.Fatalf("Expected error <%s>, got <%s>", tc.want, err.Error())
 			}
 		})
 	}
